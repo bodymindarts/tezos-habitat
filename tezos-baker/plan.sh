3b994cccf9f0e5fd23c0a0ae886fb053758f994c
Justin Carter <justin@starkandwayne.com>
2017-06-20 15:36:36 +0200

    Add baker

---
+++ tezos-baker/plan.sh
@@ -0,0 +1,32 @@
+# pkg_description="Some description."
+# pkg_upstream_url="http://example.com/project-name"
+pkg_name=tezos-baker
+pkg_origin=misthosio
+pkg_version="2017-06-06"
+# pkg_license=('Apache-2.0')
+pkg_deps=(misthosio/tezos core/iana-etc core/git)
+pkg_bin_dirs=(bin)
+pkg_svc_user=root
+pkg_svc_group=root
+
+pkg_binds=(
+  [node]="rpc_port"
+)
+
+
+do_download() {
+  return 0
+}
+
+do_verify() {
+  return 0
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  return 0
+}
+
