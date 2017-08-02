657fe0b2b4e76640ffa1f0e71f5f2ccdd489678b
Justin Carter <justin@starkandwayne.com>
2017-08-02 12:05:12 +0200

    Add tezos-testnode

---
+++ tezos-testnode/plan.sh
@@ -0,0 +1,32 @@
+# pkg_description="Some description."
+# pkg_upstream_url="http://example.com/project-name" 
+pkg_name=tezos-testnode
+pkg_origin=misthosio
+pkg_version="2017-08-01"
+# pkg_license=('Apache-2.0')
+pkg_deps=(misthosio/tezos core/iana-etc)
+pkg_bin_dirs=(bin)
+pkg_svc_user=root
+pkg_svc_group=root
+
+pkg_exports=(
+  [net_port]=net_port
+  [rpc_port]=rpc_port
+)
+pkg_exposes=(net_port rpc_port)
+
+do_download() {
+  return 0
+}
+
+do_verify() {
+  return 0
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  return 0
+}
